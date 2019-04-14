daa7ce175f44e5b3ce02af049f15efd6c1e024da
Jose Antonio Medina Cuevas <jamc34@msn.com>
2019-04-14 09:53:00 +0200

    Added option with compiled regexes Added fast option excluding groups

--- modulo1/caso_practico/calcula_sentimiento.py
+++ modulo1/caso_practico/calcula_sentimiento.py
@@ -9,7 +9,8 @@ def load_fichero_sentimientos(path):
 	    fichero=open(path)
 	    for linea in fichero:
 	        termino,valor=linea.split("\t")
-	        valores[termino]=int(valor)
+	        pattern="\W"+termino+"\W"
+	        valores[termino]={'valor':int(valor), 'regex': re.compile(pattern,re.IGNORECASE)}
 	    fichero.close()
     else:
         print("El fichero ",path," no existe.")
@@ -34,17 +35,40 @@ def load_fichero_tweets(path):
 def valorar_tweets(tweets, valores):
     for tweet in tweets:
         valor_tweet = 0    
-        for termino in valores:
-            if re.search(termino, tweet) != None:
-                valor_tweet+=valores[termino]
-                print("Encontrado termino ", termino, " en tweet: ", tweet, " con valor ", valores[termino])
+        for termino, termdata in valores.items():
+            if termdata['regex'].search(tweet) != None:
+                valor_tweet+=termdata['valor']
+                print("Encontrado termino ", termino, " en tweet: ", tweet, " con valor ", termdata['valor'])
+        print("EL SIGUIENTE TWEET: '", tweet, "' TIENE UN SENTIMIENTO ASOCIADO DE: ", valor_tweet)            
+
+# No tiene en cuenta grupos de palabras
+def valorar_tweets_fast(tweets, valores):
+    for tweet in tweets:
+        valor_tweet = 0    
+        words=re.split(r'\W+',tweet)
+        for word in words:
+            if word in valores:
+                valor_tweet+=valores[word]['valor']
+                print("Encontrado termino ", word, " en tweet: ", tweet, " con valor ", valores[word]['valor'])
         print("EL SIGUIENTE TWEET: '", tweet, "' TIENE UN SENTIMIENTO ASOCIADO DE: ", valor_tweet)            
 
 path_fichero_tweets=argv[1]
 path_fichero_sentimientos=argv[2]
+options=[]
+if len(argv) > 3:
+    options_string=argv[3]
+    options=re.split(',',options_string)
+
 
 valores=load_fichero_sentimientos(path_fichero_sentimientos)
 tweets=load_fichero_tweets(path_fichero_tweets)
 # print("Tweets: ", tweets );
 # print ("Valores: ", valores.items())
-valorar_tweets(tweets, valores)
+if 'fast' in options:
+    print("Do it fast")
+    valorar_tweets_fast(tweets, valores)
+else:
+    print("Do it well")
+    valorar_tweets(tweets, valores)
+
+
